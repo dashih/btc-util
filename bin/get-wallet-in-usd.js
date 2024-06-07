2fd415f30e5c9f6a9ab2ddd1cc0134a97a5ef1e7
Danny Shih <dashih@users.noreply.github.com>
2024-06-06 22:36:36 -0700

    adds script to get wallet balance in USD

---
+++ bin/get-wallet-in-usd.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const execSync = require('child_process').execSync;
+
+(async () => {
+    const resp = await fetch('https://api.coinbase.com/v2/prices/BTC-USD/buy');
+    const data = await resp.json();
+    const usdToBtc = data['data']['amount'];
+    const walletData = execSync('bitcoin-cli getwalletinfo').toString();
+    const walletInBtc = JSON.parse(walletData)['balance'];
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+    console.log(formatter.format(usdToBtc * walletInBtc));
+})();
