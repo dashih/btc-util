43401976325b80cb1f387c7a8c8d74d7542ff8d2
danny <danny@dannyshih.net>
2024-11-12 16:04:33 -0800

    tweaks formatting

--- bin/report-btc-node.js
+++ bin/report-btc-node.js
@@ -32,14 +32,13 @@ const execSync = require('child_process').execSync;
     let changePrint = `${formatter.format(change)}`;
     const changePercPrint = `${(changePerc * 100).toFixed(1)}%`;
     if (change > 0) {
-        changePrint = `\x1b[92m${changePrint} (+${changePercPrint})\x1b[0m`;
+        changePrint = `\x1b[92m+${changePrint} (+${changePercPrint})\x1b[0m`;
     } else {
         changePrint = `\x1b[91m${changePrint} (${changePercPrint}%)\x1b[0m`;
     }
 
     const currentValue = `\x1b[93m${formatter.format(walletInUsd)}\x1b[0m`;
-    const rawBtc = `\x1b[33m${walletInBtc} BTC\x1b[0m`;
-    console.log(`${currentValue} | ${rawBtc} | ${formatter.format(usdToBtc)} per BTC | ${changePrint}`);
+    console.log(`${currentValue} (${walletInBtc} BTC) | ${changePrint} | ${formatter.format(usdToBtc)} per BTC`);
 
     const nodeUptimePrint = `${Math.floor(nodeUptimeS / (24 * 60 * 60))} days ${Math.round((nodeUptimeS % (24 * 60 * 60) / (60 * 60)))} hours`
     console.log(`${nodeUptimePrint} | ${numInbound} inbound | ${score} score`);
