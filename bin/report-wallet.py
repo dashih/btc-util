e79c81bf2b868f0be6269692e9a0a6f71703b9b1
danny <danny@dannyshih.net>
2025-03-10 17:37:19 -0700

    ports scripts from nodejs to python

--- bin/report-wallet.py
+++ bin/report-wallet.py
@@ -6,11 +6,10 @@ from subprocess import check_output
 
 cost_basis = 67809.13
 
-usd_to_btc = convert_btc_to_usd(2)
-
 wallet_data = check_output("bitcoin-cli getwalletinfo", shell=True, text=True)
 wallet_in_btc = loads(wallet_data)["balance"]
-wallet_in_usd = usd_to_btc * wallet_in_btc
+usd_to_btc = convert_btc_to_usd(1)
+wallet_in_usd = wallet_in_btc * usd_to_btc
 change = wallet_in_usd - cost_basis
 change_perc = (wallet_in_usd - cost_basis) / cost_basis
 
@@ -19,4 +18,22 @@ network_node_json = loads(network_node_data)
 num_inbound = network_node_json["connections_in"]
 score = network_node_json["localaddresses"][0]["score"]
 
-uptime = check_output("bitcoin-cli uptime", shell=True, text=True)
+uptime_s = int(check_output("bitcoin-cli uptime", shell=True, text=True))
+days = uptime_s // (24 * 60 * 60)
+remaining_s = uptime_s % (24 * 60 * 60)
+hours = remaining_s // (60 * 60)
+remaining_s = remaining_s % (60 * 60)
+mins = remaining_s // 60
+
+# Colors
+change_print = f"${change:,.2f}"
+if change > 0:
+    change_print = f"\x1b[92m+{change_print} (+{round(change_perc * 100, 2)}%)\x1b[0m"
+else:
+    change_print = f"\x1b[91m${change_print} ({round(change_perc * 100, 2)}%)\x1b[0m"
+
+wallet_in_usd_print = f"\x1b[93m${wallet_in_usd:,.2f}\x1b[0m"
+
+# Final output
+print(f"{wallet_in_usd_print} ({wallet_in_btc} BTC) | {change_print} | ${usd_to_btc:,.2f} per BTC")
+print(f"{days}d {hours}h {mins}m | {num_inbound} inbound | {score} score")
